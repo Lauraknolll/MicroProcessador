3d0aaf8eb0f4af6ee9e0a5d246d9f02af61373f2
Laura <laura.k.g.o@gmail.com>
2025-09-13 15:42:48 -0300

    Só testando se tá funcionando ok.

--- ULA/ULA.vhd
+++ ULA/ULA.vhd
@@ -7,21 +7,22 @@ entity ULA is
         in_A, in_B : in unsigned(15 downto 0);
         Sel0, Sel1 : in std_logic;
         Resultado : out unsigned(15 downto 0)
+        Carry, Overflow, Zero, Sinal : out std_logic;
     );
 end entity;
 
 architecture struct of ULA is
 
-    signal soma, subtracao, resand, resor : unsigned(15 downto 0);
+    signal ResSoma, ResSubtracao, ResAND, ResOR : unsigned(15 downto 0);
 begin
-    soma <= in_A + in_B;
-    subtracao <= in_A - in_B;
-    resand <= in_A AND in_B;
-    resor <= in_A OR in_B;
+    ResSoma <= in_A + in_B;
+    ResSubtracao <= in_A - in_B;
+    ResAND <= in_A AND in_B;
+    ResOR <= in_A OR in_B;
 
-    resultado <= soma      when (Sel0 = '0' AND Sel1 = '0') else
-                 subtracao when (Sel0 = '1' AND Sel1 = '0') else
-                 resand    when (Sel0 = '0' AND Sel1 = '1') else
-                 resor     when (Sel0 = '1' AND Sel1 = '1') else
+    resultado <= ResSoma      when (Sel0 = '0' AND Sel1 = '0') else
+                 ResSubtracao when (Sel0 = '1' AND Sel1 = '0') else
+                 ResAND    when (Sel0 = '0' AND Sel1 = '1') else
+                 ResOR     when (Sel0 = '1' AND Sel1 = '1') else
                  "0000000000000000";
 end architecture;
