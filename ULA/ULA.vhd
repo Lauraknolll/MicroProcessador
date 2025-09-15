17697fd87d6137ca5e9230f73fe84b1ca8ecdb3c
Bruna Teixeira <brume.teix@gmail.com>
2025-09-15 18:24:41 -0300

    Adicionei testes (_tb), as flags: Zero, Overflow e Negativo (testadas).

--- ULA/ULA.vhd
+++ ULA/ULA.vhd
@@ -15,14 +15,47 @@ architecture struct of ULA is
 
     signal ResSoma, ResSubtracao, ResAND, ResOR : unsigned(15 downto 0);
 begin
-    ResSoma <= in_A + in_B;
+    ResSoma      <= in_A + in_B;
     ResSubtracao <= in_A - in_B;
-    ResAND <= in_A AND in_B;
-    ResOR <= in_A OR in_B;
+    ResAND       <= in_A AND in_B;
+    ResOR        <= in_A OR in_B;
 
+    -------Carry <= '1' when (((ResSoma<in_A) OR (ResSoma<in_B)) AND (Sel0 = '0' AND Sel1 = '0') ) else
+    --Carry <= '1' when (((in_A(15)='0') AND (in_B(15)='0')) AND (Sel0 = '0' AND Sel1 = '0') AND (ResSoma(15)='1')) else
+           -- '0';
+    Carry<='0';
+   --Overflow <= '1' when (((in_A(15)='0')AND (in_B(15)='0')) AND ((in_A + in_B)(15)='1')) else
+                --'1' when (((in_A(15)='1')AND (in_B(15)='1')) AND ((in_A + in_B)(15)='0')) else
+               -- '0';
+    Overflow <= '1' when ((in_A(15) = '0') and (in_B(15) = '0') and (ResSoma(15) = '1') and (Sel0 = '0' AND Sel1 = '0') ) else
+                '1' when ((in_A(15) = '1') and (in_B(15) = '1') and (ResSoma(15) = '0') and (Sel0 = '0' AND Sel1 = '0')) else
+                '1' when ((in_A(15) = '0') and (not(in_B(15)) = '0') and (ResSubtracao(15) = '1') and (Sel0 = '1' AND Sel1 = '0') ) else
+                '1' when ((in_A(15) = '1') and (not(in_B(15)) = '1') and (ResSubtracao(15) = '0') and (Sel0 = '1' AND Sel1 = '0')) else
+                '0';
+        
     resultado <= ResSoma      when (Sel0 = '0' AND Sel1 = '0') else
                  ResSubtracao when (Sel0 = '1' AND Sel1 = '0') else
-                 ResAND    when (Sel0 = '0' AND Sel1 = '1') else
-                 ResOR     when (Sel0 = '1' AND Sel1 = '1') else
+                 ResAND       when (Sel0 = '0' AND Sel1 = '1') else
+                 ResOR        when (Sel0 = '1' AND Sel1 = '1') else
                  "0000000000000000";
+   -- Sinal<= (in_A + in_B)(15)      when (Sel0 = '0' AND Sel1 = '0') else
+     --       (in_A - in_B)(15) when (Sel0 = '1' AND Sel1 = '0') else
+       --     (in_A AND in_B)(15)      when (Sel0 = '0' AND Sel1 = '1') else
+         --   (in_A OR in_B)(15)        when (Sel0 = '1' AND Sel1 = '1') else
+           -- '0';
+    Sinal<= ResSoma(15)      when (Sel0 = '0' AND Sel1 = '0') else
+            ResSubtracao(15) when (Sel0 = '1' AND Sel1 = '0') else
+            ResAND(15)      when (Sel0 = '0' AND Sel1 = '1') else
+            ResOR(15)        when (Sel0 = '1' AND Sel1 = '1') else
+            '0';
+     Zero <= '1'              when (Sel0 = '0' AND Sel1 = '0' AND (ResSoma=0)) else
+            '1'              when (Sel0 = '1' AND Sel1 = '0'AND (ResSubtracao=0)) else
+            '1'              when (Sel0 = '0' AND Sel1 = '1'AND (ResAND=0)) else
+            '1'              when (Sel0 = '1' AND Sel1 = '1'AND (REsOR=0)) else
+            '0';
+    --Zero <= '1'              when (Sel0 = '0' AND Sel1 = '0' AND ((in_A + in_B)=0)) else
+      --      '1'              when (Sel0 = '1' AND Sel1 = '0'AND ((in_A - in_B)=0)) else
+        --    '1'              when (Sel0 = '0' AND Sel1 = '1'AND ((in_A AND in_B)=0)) else
+          --  '1'              when (Sel0 = '1' AND Sel1 = '1'AND ((in_A OR in_B)=0)) else
+            --'0';
 end architecture;
--- ULA/ULA_tb.vhd
+++ ULA/ULA_tb.vhd
@@ -11,20 +11,46 @@ component ULA is
  port(
     in_A, in_B : in unsigned(15 downto 0);
     Sel0, Sel1 : in std_logic;
-    Resultado : out unsigned(15 downto 0)
+    Resultado : out unsigned(15 downto 0);
+    Carry, Overflow, Zero, Sinal : out std_logic
 );
 end component;
 
 signal in_a, in_b, res : unsigned(15 downto 0);
-signal sel0, sel1 : std_logic;
+signal sel0, sel1, carry, overflow, zero, negativo : std_logic;
 
 begin
-    uut : ULA port map (in_A => in_a, in_B => in_b, Sel0 => sel0, Sel1 => sel1, Resultado => res);
+    uut : ULA port map (in_A => in_a, in_B => in_b, Sel0 => sel0, Sel1 => sel1, Resultado => res,
+                        Carry => carry, Overflow => overflow, Zero => zero, Sinal => negativo);
     process
     begin
         --soma
         in_A <= "0000000000000001";
         in_B <= "0000000000000010";
+        Sel0 <= '0';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --soma
+        in_A <= "1111111111111001";
+        in_B <= "1111111000000010";
+        Sel0 <= '0';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --soma
+        in_A <= "0110000000000001";
+        in_B <= "0111111110000010";
+        Sel0 <= '0';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --soma negativos
+        in_A <= "1000000001100001";
+        in_B <= "1111011100000010";
+        Sel0 <= '0';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --soma negativo e positivo
+        in_A <= "1111000000001001";
+        in_B <= "0000001100000010";
         Sel0 <= '0';
         Sel1 <= '0';
         wait for 50 ns;
@@ -34,14 +60,39 @@ begin
         Sel0 <= '1';
         Sel1 <= '0';
         wait for 50 ns;
+        --subtracao negativos 
+        in_A <= "1000001100001110";
+        in_B <= "1111111111101001";
+        Sel0 <= '1';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --subtracao
+        in_A <= "1000000000000000";
+        in_B <= "1111000000101001";
+        Sel0 <= '1';
+        Sel1 <= '0';
+        wait for 50 ns;
+        --subtracao
+        in_A <= "1000000000000000";
+        in_B <= "0111111100101001";
+        Sel0 <= '1';
+        Sel1 <= '0';
+        wait for 50 ns;
         --and
         in_A <= "0000000000000001";
         in_B <= "0000000000000001";
         Sel0 <= '0';
         Sel1 <= '1';
         wait for 50 ns;
+        --and com números diferentes
         in_A <= "0000000100101000";
         in_B <= "0000000100001011";
+        Sel0 <= '0';
+        Sel1 <= '1';
+        wait for 50 ns;
+        --and com números diferentes
+        in_A <= "1110000100101000";
+        in_B <= "1000000100001011";
         Sel0 <= '0';
         Sel1 <= '1';
         wait for 50 ns;
@@ -53,6 +104,11 @@ begin
         wait for 50 ns;
         in_A <= "0000000000000000";
         in_B <= "0000000100000011";
+        Sel0 <= '1';
+        Sel1 <= '1';
+        wait for 50 ns;
+        in_A <= "0000000000000000";
+        in_B <= "1110000100000011";
         Sel0 <= '1';
         Sel1 <= '1';
         wait for 50 ns;
