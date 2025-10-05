3fe12fd03d3754bf152ed501a8495d8113367dc7
Laura <laura.k.g.o@gmail.com>
2025-10-05 09:32:09 -0300

    Começando o testebench.

--- TopLevel.vhd
+++ TopLevel.vhd
@@ -5,12 +5,12 @@ use ieee.numeric_std.all;
 entity TopLevel is
     port (
         clock : in std_logic;
-        dado_escrita_banco : in std_logic;
+        dado_escrita_banco : in unsigned(15 downto 0);
         reset_b, reset_acc : in std_logic; --o reset dos dois acumuladores é o mesmo??
         qual_reg_escreve, qual_reg_le : in unsigned(3 downto 0); --no banco
         escreve_banco : in std_logic;
 
-        escolhe_acc : in std_logic; --se for 1 escolhe o accA, se for 0 escolhe o accB
+        escolhe_acc0 : in std_logic; --se for 1 escolhe o accA, se for 0 escolhe o accB
 
         sel0, sel1 : in std_logic; --operações da ula
         carry, overflow, zero, sinal : out std_logic
@@ -53,13 +53,15 @@ architecture struct of TopLevel is
     end component;
 
     signal banco_ula, ula_accs, acc0_ula, acc1_ula, accs_ula : unsigned(15 downto 0);
+    signal escolhe_acc1 : std_logic;
 
 begin
 
     uut0 : BancoReg port map (clk_b => clock, rst_b => reset_b, wr_en => escreve_banco, sel_reg_wr => qual_reg_escreve, sel_reg_rd => qual_reg_le, data_wr => dado_escrita_banco, data_out_r1 => banco_ula);
-    uutA : reg16bits port map (clk => clock, rst => reset_acc, wr_en => escolhe_acc, data_in => ula_accs, data_out => acc0_ula); --acumulador A/0
-    uutB : reg16bits port map (clk => clock, rst => reset_acc, wr_en => not(escolhe_acc), data_in => ula_accs, data_out => acc1_ula); --acumulador B/1
-    accs_ula <= acc0_ula when escolhe_acc else acc1_ula; --será que é o mesmo sinal do wr deles?
+    uutA : reg16bits port map (clk => clock, rst => reset_acc, wr_en => escolhe_acc0, data_in => ula_accs, data_out => acc0_ula); --acumulador A/0
+    escolhe_acc1 <= not (escolhe_acc0);
+    uutB : reg16bits port map (clk => clock, rst => reset_acc, wr_en => escolhe_acc1, data_in => ula_accs, data_out => acc1_ula); --acumulador B/1
+    accs_ula <= acc0_ula when escolhe_acc0 = '1' else acc1_ula; --será que é o mesmo sinal do wr deles?
     uut1 : ULA port map (in_A => banco_ula, in_B => accs_ula, Sel0 => sel0, Sel1 => sel1, Resultado => ula_accs, Carry => carry, Overflow => overflow, Zero => zero, Sinal => sinal);
 
 end struct ; 
--- TopLevel_tb.vhd
+++ TopLevel_tb.vhd
@@ -10,12 +10,12 @@ architecture struct of TopLevel_tb is
     component TopLevel is
     port (
         clock : in std_logic;
-        dado_escrita_banco : in std_logic;
+        dado_escrita_banco : in unsigned(15 downto 0);
         reset_b, reset_acc : in std_logic; --o reset dos dois acumuladores é o mesmo??
         qual_reg_escreve, qual_reg_le : in unsigned(3 downto 0); --no banco
         escreve_banco : in std_logic;
 
-        escolhe_acc : in std_logic; --se for 1 escolhe o accA, se for 0 escolhe o accB
+        escolhe_acc0 : in std_logic; --se for 1 escolhe o accA, se for 0 escolhe o accB
 
         sel0, sel1 : in std_logic; --operações da ula
         carry, overflow, zero, sinal : out std_logic
@@ -23,12 +23,23 @@ architecture struct of TopLevel_tb is
     );
     end component;
 
-    signal 
+    signal clock, reset_b, reset_acc, escreve_banco, escolhe_acc0, sel0, sel1, carry, overflow, zero, sinal : std_logic;
+    signal dado_escrita_banco : unsigned(15 downto 0);
+    signal qual_reg_escreve, qual_reg_le : unsigned(3 downto 0);
 
 begin
+    uut : TopLevel port map (clock => clock, dado_escrita_banco => dado_escrita_banco, reset_b => reset_b, reset_acc => reset_acc, qual_reg_escreve => qual_reg_escreve, qual_reg_le => qual_reg_le,
+    escreve_banco => escreve_banco, escolhe_acc0 => escolhe_acc0, sel0 => sel0, sel1 => sel1, carry => carry, overflow => overflow, zero => zero, sinal => sinal);
 
-    process
+    reset_global: process
     begin
+        reset_b <= '1';
+        reset_acc <= '1';
+        wait for 100 ns; -- espera 2 clocks, pra garantir
+        reset_b <= '0';
+        reset_acc <= '0';
+        wait for 100 ns;
+        wait;
     end process;
 
 end architecture;
